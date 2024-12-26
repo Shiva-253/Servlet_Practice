e8d47e332232ddaaddda8c9fbd178e6aa6837144
shivamk <shivamk@P2021-PC00118.prorigo.com>
2024-12-26 17:47:55 +0530

    This is updated code with RequestDispatcher.

--- Form/src/main/java/com/prorigo/training/RegisterServlet.java
+++ Form/src/main/java/com/prorigo/training/RegisterServlet.java
@@ -2,6 +2,8 @@ package com.prorigo.training;
 
 import java.io.*;
 
+import jakarta.servlet.RequestDispatcher;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,30 +16,33 @@ public class RegisterServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
 
-		String name = request.getParameter("user_name");
-		String password = request.getParameter("user_password");
-		String email = request.getParameter("user_email");
-		String gender = request.getParameter("user_gender");
-		String course = request.getParameter("user_course");
+	    String name = request.getParameter("user_name");
+	    String password = request.getParameter("user_password");
+	    String email = request.getParameter("user_email");
+	    String gender = request.getParameter("user_gender");
+	    String course = request.getParameter("user_course");
 
-		String cond = request.getParameter("condition");
+	    String cond = request.getParameter("condition");
 
-		if(cond.equals("checked")) {
-			out.println("<h2>Name : " + name+ "</h2>");
-			out.println("<h2>Email : "+ email+ "</h2>");
-			out.println("<h2>Password : "+ password+ "</h2>");
-			out.println("<h2>Gender : "+ gender+ "</h2>");
-			out.println("<h2>Course : "+ course + "</h2>");
-		}
-		else {
-			out.println("<h2>You have not accepted terms and conditions</h2>");
-		}
-
-
+	    if (cond != null && cond.equals("checked")) {
+	        out.println("<h2>Name : " + name + "</h2>");
+	        out.println("<h2>Email : " + email + "</h2>");
+	        out.println("<h2>Password : " + password + "</h2>");
+	        out.println("<h2>Gender : " + gender + "</h2>");
+	        out.println("<h2>Course : " + course + "</h2>");
+	        
+	        RequestDispatcher rd = request.getRequestDispatcher("Success");
+	        rd.forward(request, response);
+	    } else {
+	        out.println("<h2>You have not accepted terms and conditions</h2>");
+	        RequestDispatcher rd = request.getRequestDispatcher("index.html");
+	        rd.include(request, response);
+	    }
 	}
+
 
 }
